e50c0451fbce47044024bcb044cfc3c5e82014cc
Saurabh <sauravvishwakarma030@gmail.com>
2024-05-07 14:02:03 +0530

    fix base model mixin

--- api/database/base_model.py
+++ api/database/base_model.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import declarative_mixin, Mapped, declared_attr, mapped_column
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Integer, String, DateTime
 from nanoid import generate
 from datetime import datetime
 from .db import Base
@@ -11,7 +11,7 @@ class MyMixin:
         String(12), default=lambda: generate(size=12), primary_key=True, unique=True
     )
     created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now)
-    updated_at: Mapped[DateTime] = Column(
+    updated_at: Mapped[DateTime] = mapped_column(
         DateTime, default=datetime.now, onupdate=datetime.now
     )
 
@@ -21,8 +21,6 @@ class MyMixin:
 
     __table_args__ = {"mysql_engine": "InnoDB"}
     __mapper_args__ = {"always_refresh": True}
-
-    id = Column(Integer, primary_key=True)
 
 
 class BaseModel(MyMixin, Base): ...
